ea775dd88527b45ba93e4c7d95bab2985533b146
Trunin Dmitry <dmitry.trunin@corp.mail.ru>
2019-02-08 14:05:37 +0300

    Stub for FireView.

---
+++ app/src/main/java/ru/ok/technopolis/firedemoapp/FireView.java
@@ -0,0 +1,24 @@
+package ru.ok.technopolis.firedemoapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FireView extends View {
+
+    public FireView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+    }
+}
