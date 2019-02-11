0f357677e75480f60df992bfbb7b9ea10e3ed50c
Trunin Dmitry <dmitry.trunin@corp.mail.ru>
2019-02-11 13:36:02 +0300

    Don't use 2-dimensional array.

--- app/src/main/java/ru/ok/technopolis/firedemoapp/FireView.java
+++ app/src/main/java/ru/ok/technopolis/firedemoapp/FireView.java
@@ -53,7 +53,7 @@ public class FireView extends View {
             0xffFFFFFF
     };
 
-    private int[][] firePixels;
+    private int[] firePixels;
     private int fireWidth;
     private int fireHeight;
     private int[] bitmapPixels;
@@ -71,10 +71,10 @@ public class FireView extends View {
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         fireWidth = w;
         fireHeight = h;
-        firePixels = new int[fireWidth][fireHeight];
+        firePixels = new int[fireWidth * fireHeight];
 
         for (int x = 0; x < fireWidth; x ++) {
-            firePixels[x][fireHeight - 1] = firePalette.length - 1;
+            firePixels[(fireHeight - 1) * fireWidth + x] = firePalette.length - 1;
         }
         bitmap = Bitmap.createBitmap(fireWidth, fireHeight, Bitmap.Config.RGB_565);
     }
@@ -92,10 +92,9 @@ public class FireView extends View {
             bitmapPixels = new int[pixelCount];
         }
 
-
-        for (int x = 0; x < fireWidth; x++) {
-            for (int y = 0; y < fireHeight; y++) {
-                int temperature = firePixels[x][y];
+        for (int y = 0; y < fireHeight; y++) {
+            for (int x = 0; x < fireWidth; x++) {
+                int temperature = firePixels[x + y * fireWidth];
                 if (temperature < 0) {
                     temperature = 0;
                 }
@@ -118,7 +117,7 @@ public class FireView extends View {
                 int dst_x = Math.min(fireWidth - 1, Math.max(0, x + rand_x - 1));
                 int dst_y = Math.min(fireHeight - 1, y + rand_y);
                 int deltaFire = -(rand_x & 1);
-                firePixels[x][y] = Math.max(0, firePixels[dst_x][dst_y] + deltaFire);
+                firePixels[x + y * fireWidth] = Math.max(0, firePixels[dst_x + dst_y * fireWidth] + deltaFire);
             }
         }
     }
